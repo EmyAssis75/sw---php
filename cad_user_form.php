baa00be66ddaa25a658341ce616fecda1e4e7b1f
EmyAssis75 <166054739+EmyAssis75@users.noreply.github.com>
2024-09-04 14:57:08 -0300

    cad_user_form.php

---
+++ cad_user_form.php
@@ -0,0 +1,44 @@
+<form method="post" action="cad_user_add.php" id="formlogin" name="formlogin">
+    <fieldset id="fie">
+        <div align="center">
+            <br />
+            <table width="41%" border="0" align="center">
+                <tr>
+                    <td> </td>
+                    <td>
+                        <legend>Cadastro Usuário</legend>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div align="right">NOME :</div>
+                    </td>
+                    <td><input type="text" name="nome" id="nome" /></td>
+                </tr>
+                <tr>
+                    <td>
+                        <div align="right">SENHA :</div>
+                    </td>
+                    <td><input type="password" name="senha" id="senha" /></td>
+                </tr>
+                <tr>
+                    <td>
+                        <div align="right">TELEFONE :</div>
+                    </td>
+                    <td><input type="text" name="telefone" id="telefone" /></td>
+                </tr>
+                <tr>
+                    <td>
+                        <div align="right">IMAGEM :</div>
+                    </td>
+                    <td><input type="text" name="imagem" id="imagem" /></td>
+                </tr>
+
+                <tr>
+                    <td> </td>
+                    <td><input name="submit" type="submit" value="Criar " /></td>
+                </tr>
+            </table>
+        </div>
+    </fieldset>
+</form>

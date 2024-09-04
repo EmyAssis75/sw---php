f111026a5c8489ea07cfb639df21744d1a55316a
EmyAssis75 <166054739+EmyAssis75@users.noreply.github.com>
2024-09-04 14:57:54 -0300

    cad_user_add.php

---
+++ cad_user_add.php
@@ -0,0 +1,17 @@
+<?php
+require('conect.php');
+try {
+    $stmt = $pdo->prepare('INSERT INTO seubanco.usuario (`nome`,`senha`,`telefone`,`imagem` ) VALUES (:nome,:senha,:telefone,:imagem)');
+    $stmt->execute(array(
+        ':nome' => $nome,
+        ':senha' => md5($senha),
+        ':telefone' => $telefone,
+        ':imagem' => $imagem
+
+    ));
+
+} catch (PDOException $e) {
+    echo 'Erro:' . $e->getMessage();
+    echo ("Não conectado");
+}
+?>

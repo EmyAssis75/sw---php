c43d552181f17625c120f4f30808aeead3a8a700
EmyAssis75 <166054739+EmyAssis75@users.noreply.github.com>
2024-09-23 07:43:11 -0300

    footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+<table width="730" border="0" align="center">
+    <td width="300"><b> WILIAM® <b> - Todos os direitos Reservados 2022</td>
+    <td width="50"><a href="index.php" target="_parent" ><img src="assets/facebook.png" width="25px" heigth="25px' alt-"user login" longdesc-"index.php" &nbsp /></a> </td> 
+    <td width="50"><a href="index.php" target="_parent" ><img src="assets/youtube.png" width='25px" heigth="25px' alt="user login" longdesc="index.php" &nbsp /></a> </td> 
+    <td width="50"><a href="index.php" target="_parent" ><img src="assets/instagram.png" width="25px" heigth="25px' alt-"user login" longdesc-"index.php" &nbsp /></a> </td> 
+    <td width="50"><a href="index.php" target="_parent" ><img src="assets/twitter.png" width="25px" heigth='25px' alt="user login" longdesc="index.php" &nbsp /></a> </td> I
+    </table>

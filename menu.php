4d9452c158e8ea7745c8eb7553b35674408295b1
EmyAssis75 <166054739+EmyAssis75@users.noreply.github.com>
2024-09-25 14:44:38 -0300

    menu.php

---
+++ menu.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <title>SISTEMA</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <img class="avatar" src="assets/robot.png" width='65px' heigth='65px' alt="user login" />
+
+            <a class="navbar-brand" href="#"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="body.php" target="mainFrame">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="cad_user_add_form.php" target="mainFrame">CADASTRAR</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="cad_user_lst_form.php" target="mainFrame">LISTAR</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=" cad_user_del_form.php" target="mainFrame">REMOVER</a>
+                    </li>
+                   <li class="nav-item">
+                        <a class="nav-link" href=" cad_user_del_form.php" target="mainFrame">ALTERAR</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=" cad_user_del_form.php" target="mainFrame">MODAL</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="./sys/contact/read.php" target="mainFrame">READ</a>
+                    </li>
+                </ul>
+            </div>
+
+
+        </div>
+
+
+        <table width="430" border="0">
+        <td width="200"><?php
+
+//$timezone = new DateTimeZone('America/Sao_Paulo');
+//$teste = new DateTime('now', $timezone);
+//$agora = date('d/m/Y H:i');
+//echo $agora;
+
+
+$timezone = new DateTimeZone('America/Sao_Paulo');
+$teste = new DateTime('now', $timezone);
+$agora = $teste->format('d/m/Y H:i');
+echo $agora;
+
+?></td>
+            <td width="270">
+                <?php
+                //require('conect.php');
+                //require('conectar.php');
+                session_start();
+                echo "<b> Usuário: &nbsp</b>"  .  $_SESSION["login"] . "<br>";
+                ?> </td>
+            <td width="108"><a href="index.php" target="_parent"><img src="assets/logout.png" width='25px' heigth='25px' alt="user login" longdesc="index.php" &nbsp /></a> </td>
+            <td width="10"></td>
+
+
+        </table>
+
+        <a> </a>
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+</body>
+
+</html>

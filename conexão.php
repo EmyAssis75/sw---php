d34eb4d61115ccc4f15a6d9128b23262d0567db5
EmyAssis75 <166054739+EmyAssis75@users.noreply.github.com>
2024-09-25 15:00:13 -0300

    conexão.php

---
+++ conexão.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-
+1" />
+<title>SGEP
+-
+ETEC</title>
+<link rel="stylesheet" href="styles.css">
+<style>
+body {
+font-family: Arial, sans-serif;
+margin: 0;
+}
+frameset, frame {
+} </style>
+</head>
+border: none;
+<frameset rows="80,*,80" frameborder="no" border="0" framespacing="0"> <frame src="menu.php" name="topFrame" scrolling="no"
+noresize="noresize" id="topFrame" />
+<frame src="body.php" name="mainFrame" id="mainFrame" /> <frame src="footer.php" name="bottomFrame" scrolling="no" noresize="noresize" id="bottomFrame" />
+</frameset>
+<noframes>
+<body>
+</body>
+</noframes>
+</html>

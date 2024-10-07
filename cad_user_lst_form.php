a3a609e1b5492b28829f34291c2d1c1ef3fae675
EmyAssis75 <166054739+EmyAssis75@users.noreply.github.com>
2024-10-07 07:50:55 -0300

    cad_user_lst_form.php

---
+++ cad_user_lst_form.php
@@ -0,0 +1,51 @@
+<fieldset id="fie">
+  <div align="center">
+  <?php
+	session_start();
+	echo "Usuário Conectado: " . $_SESSION["login"] . "<br>";
+	?>
+    <legend></legend>
+    <label></label>
+	<label></label>
+    <table width="41%" border="0" align="center">
+
+      <tr>
+        <?php
+        require ('conect.php');
+
+        $stmt = $pdo->query('select * from seubanco.usuario order by id asc;');
+
+		echo "<table border='1px'>";
+		echo "<tr><th width='30px' align='center'>Id</th><th width='100px'>Usuário</th><th width='250px'>Senha</th><th width='100px'>Telefone</th><th width='100px'>Imagem</th><tr>";
+
+        while ($linha = $stmt->fetch(PDO::FETCH_ASSOC)) {
+			
+			echo "<tr>";
+			echo "<td align='center'>". $linha['id']."</td>";
+			echo "<td>". $linha['nome']."</td>";
+			echo "<td>". $linha['senha']."</td>";
+			echo "<td>". $linha['telefone']."</td>";		
+			// buscando a na pasta imagem
+			if (empty($linha['imagem'])){
+				$imagem = 'SemImagem.png';
+			}else{
+				$imagem = $linha['imagem'];
+			}
+			$id = base64_encode($linha['id']);
+			echo "<td align='center'><img src='$imagem' width='50px' heigth='50px'></a>";
+			
+			
+			echo "</tr>";
+			
+			echo "<img src='{$linha['imagem']}' width=25 height=25 /> ID: {$linha['id']} - Nome: {$linha['nome']} - Senha: {$linha['senha']} - Telefone: {$linha['telefone']} - Imagem: {$linha['imagem']} - Link:  {$linha['imagem']}<br />";
+         
+        }
+
+        ?>
+
+      </tr>
+    </table>
+  </div>
+</fieldset>
+
+<td> </td>

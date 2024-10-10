d2b6144039d96871791e97673fb1ca3041617cb2
EmyAssis75 <166054739+EmyAssis75@users.noreply.github.com>
2024-10-10 14:02:51 -0300

    cad_user_upd_form.php

--- cad_user_upd_form.php
+++ cad_user_upd_form.php
@@ -1,103 +1,65 @@
 <fieldset id="fie">
-  <div align="center">
-  <?php
-	session_start();
-	echo "Usuário Conectado: " . $_SESSION["login"] . "<br>";
-	?>
-    <legend></legend>
-    <label></label>
-	<label></label>
-    <table width="41%" border="0" align="center">
-
-      <tr>
+    <div style="text-align: center;">
         <?php
-        require ('conect.php');
-
-        $stmt = $pdo->query('select * from seubanco.usuario;');
-
-		echo "<table border='1px'>";
-		echo "<tr><th width='30px' align='center'>Id</th><th width='100px'>Usuário</th><th width='250px'>Senha</th><th width='100px'>Telefone</th><th width='100px'>Imagem</th><tr>";
-
-        while ($linha = $stmt->fetch(PDO::FETCH_ASSOC)) {
-			
-			echo "<tr>";
-			echo "<td align='center'>". $linha['id']."</td>";
-			echo "<td>". $linha['nome']."</td>";
-			echo "<td>". $linha['senha']."</td>";
-			echo "<td>". $linha['telefone']."</td>";		
-			// buscando a na pasta imagem
-			if (empty($linha['imagem'])){
-				$imagem = 'SemImagem.png';
-			}else{
-				$imagem = $linha['imagem'];
-			}
-			$id = base64_encode($linha['id']);
-			echo "<td align='center'><img src='$imagem' width='50px' heigth='50px'></a>";
-			
-			
-			echo "</tr>";
-			
-			echo "<img src='{$linha['imagem']}' width=25 height=25 /> ID: {$linha['id']} - Nome: {$linha['nome']} - Senha: {$linha['senha']} - Telefone: {$linha['telefone']} - Imagem: {$linha['imagem']} - Link:  {$linha['imagem']}<br />";
-         
-        }
-
+        session_start();
+        echo "Usuário Conectado: " . htmlspecialchars($_SESSION["login"]) . "<br>";
         ?>
+        <legend>Lista de Usuários</legend>
+        <table border="1" style="margin: 0 auto;">
+            <tr>
+                <th width="30px">Id</th>
+                <th width="100px">Usuário</th>
+                <th width="100px">Telefone</th>
+                <th width="100px">Imagem</th>
+            </tr>
+            <?php
+            require('conect.php');
+            $stmt = $pdo->query('SELECT * FROM seubanco.usuario;');
 
-      </tr>
-    </table>
-  </div>
+            while ($linha = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $imagem = empty($linha['imagem']) ? 'SemImagem.png' : htmlspecialchars($linha['imagem']);
+                echo "<tr>
+                        <td align='center'>" . htmlspecialchars($linha['id']) . "</td>
+                        <td>" . htmlspecialchars($linha['nome']) . "</td>
+                        <td>" . htmlspecialchars($linha['telefone']) . "</td>
+                        <td align='center'><img src='$imagem' width='50' height='50'></td>
+                      </tr>";
+            }
+            ?>
+        </table>
+    </div>
 </fieldset>
 
-<td> </td>
-
-<form method="post" action="cad_user_upd.php" id="formlogin" name="formlogin">
-  <fieldset id="fie">
-    <div align="center">
-      <legend></legend>
-      <label></label>
-      <label></label>
-      <table width="41%" border="0" align="center">
-        <tr>
-          <td> </td>
-          <td>
+<form method="post" action="cad_user_upd.php" id="formlogin">
+    <fieldset id="fie">
+        <div style="text-align: center;">
             <legend>Alterar Usuário</legend>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div align="right">ID :</div>
-          </td>
-          <td><input type="text" name="id" id="id" /></td>
-        </tr>
-        <tr>
-          <td>
-            <div align="right">NOME :</div>
-          </td>
-          <td><input type="text" name="nome" id="nome" /></td>
-        </tr>
-        <tr>
-          <td>
-            <div align="right">SENHA :</div>
-          </td>
-          <td><input type="password" name="senha" id="senha" /></td>
-        </tr>
-        <tr>
-          <td>
-            <div align="right">TELEFONE :</div>
-          </td>
-          <td><input type="text" name="telefone" id="telefone" /></td>
-        </tr>
-        <tr>
-          <td>
-            <div align="right">IMAGEM :</div>
-          </td>
-          <td><input type="text" name="imagem" id="imagem" /></td>
-        </tr>
-        <tr>
-          <td> </td>
-          <td><input name="submit" type="submit" value="Alterar" /></td>
-        </tr>
-      </table>
-    </div>
-  </fieldset>
+            <table style="margin: 0 auto;">
+                <tr>
+                    <td><label for="id">ID:</label></td>
+                    <td><input type="text" name="id" id="id" required /></td>
+                </tr>
+                <tr>
+                    <td><label for="nome">NOME:</label></td>
+                    <td><input type="text" name="nome" id="nome" /></td>
+                </tr>
+                <tr>
+                    <td><label for="senha">SENHA:</label></td>
+                    <td><input type="password" name="senha" id="senha" /></td>
+                </tr>
+                <tr>
+                    <td><label for="telefone">TELEFONE:</label></td>
+                    <td><input type="text" name="telefone" id="telefone" /></td>
+                </tr>
+                <tr>
+                    <td><label for="imagem">IMAGEM:</label></td>
+                    <td><input type="text" name="imagem" id="imagem" /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input name="submit" type="submit" value="Alterar" /></td>
+                </tr>
+            </table>
+        </div>
+    </fieldset>
 </form>

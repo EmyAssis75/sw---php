31512e0e35a23819e634c004ff83e4de8f2665c6
EmyAssis75 <166054739+EmyAssis75@users.noreply.github.com>
2024-10-10 14:15:39 -0300

    cad_user_lst_form.php

--- cad_user_lst_form.php
+++ cad_user_lst_form.php
@@ -1,51 +1,65 @@
 <fieldset id="fie">
   <div align="center">
-  <?php
-	session_start();
-	echo "Usuário Conectado: " . $_SESSION["login"] . "<br>";
-	?>
+    <?php
+    session_start();
+    echo "Usuário Conectado: " . htmlspecialchars($_SESSION["login"]) . "<br>";
+    ?>
     <legend></legend>
     <label></label>
-	<label></label>
+    <label></label>
     <table width="41%" border="0" align="center">
-
       <tr>
         <?php
-        require ('conect.php');
+        require('conect.php');
 
-        $stmt = $pdo->query('select * from seubanco.usuario order by id asc;');
+        $stmt = $pdo->query('SELECT * FROM seubanco.usuario;');
 
-		echo "<table border='1px'>";
-		echo "<tr><th width='30px' align='center'>Id</th><th width='100px'>Usuário</th><th width='250px'>Senha</th><th width='100px'>Telefone</th><th width='100px'>Imagem</th><tr>";
+        echo "<table border='1px'>";
+        echo "<tr><th width='30px' align='center'>Id</th><th width='100px'>Usuário</th><th width='100px'>Telefone</th><th width='100px'>Imagem</th></tr>";
 
         while ($linha = $stmt->fetch(PDO::FETCH_ASSOC)) {
-			
-			echo "<tr>";
-			echo "<td align='center'>". $linha['id']."</td>";
-			echo "<td>". $linha['nome']."</td>";
-			echo "<td>". $linha['senha']."</td>";
-			echo "<td>". $linha['telefone']."</td>";		
-			// buscando a na pasta imagem
-			if (empty($linha['imagem'])){
-				$imagem = 'SemImagem.png';
-			}else{
-				$imagem = $linha['imagem'];
-			}
-			$id = base64_encode($linha['id']);
-			echo "<td align='center'><img src='$imagem' width='50px' heigth='50px'></a>";
-			
-			
-			echo "</tr>";
-			
-			echo "<img src='{$linha['imagem']}' width=25 height=25 /> ID: {$linha['id']} - Nome: {$linha['nome']} - Senha: {$linha['senha']} - Telefone: {$linha['telefone']} - Imagem: {$linha['imagem']} - Link:  {$linha['imagem']}<br />";
-         
+            echo "<tr>";
+            echo "<td align='center'>" . htmlspecialchars($linha['id']) . "</td>";
+            echo "<td>" . htmlspecialchars($linha['nome']) . "</td>";
+            echo "<td>" . htmlspecialchars($linha['telefone']) . "</td>";
+
+            // Definindo imagem padrão
+            $imagem = empty($linha['imagem']) ? 'SemImagem.png' : htmlspecialchars($linha['imagem']);
+            echo "<td align='center'><img src='$imagem' width='50px' height='50px'></td>";
+            echo "</tr>";
         }
-
+        echo "</table>";
         ?>
-
       </tr>
     </table>
   </div>
 </fieldset>
 
-<td> </td>
+<form method="post" action="cad_user_del.php" id="formlogin" name="formlogin">
+  <fieldset id="fie">
+    <div align="center">
+      <legend>Remover Usuário</legend>
+      <table width="41%" border="0" align="center">
+        <tr>
+          <td align="right">ID:</td>
+          <td><input type="text" name="id" id="id" placeholder="Digite o ID do usuário" required /></td>
+        </tr>
+        <tr>
+          <td colspan="2" align="center"><input name="submit" type="submit" value="Remover" /></td>
+        </tr>
+      </table>
+    </div>
+  </fieldset>
+</form>
+
+<?php
+// Mensagens de erro ou sucesso
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['submit'])) {
+        $id = $_POST['id'];
+        // Validação do ID
+        if (!empty($id) && is_numeric($id)) {
+    		}
+	}
+}	    
+?>
